dcda662818ab8c35aac5c213fec0d7caf37e1ccd
Fernando Henrique Ellery <fernando.ellery@cogerh.com.br>
2024-09-04 10:28:00 -0300

    commit estrutural

---
+++ src/main/java/br/com/fe/hell/apihabit/HabitApplication.java
@@ -0,0 +1,13 @@
+package br.com.fe.hell.apihabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fe/hell/apihabit/HabitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fe.hell.apihabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
